b58f830ebe88ef1ffdc344423833f04fe55dbd17
MikolajFiderek <250955@edu.p.lodz.pl>
2024-04-05 16:50:10 +0200

    dividing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ void usage(char *programName) {
     printf("Usage: %s number1 operation number2\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
+    printf("\tdiv\n");
     printf("\n");
 }
 
@@ -26,8 +27,23 @@ int main(int argc, char* argv[]) {
                 printf("%d + %d = %d\n", number1, number2, result);
                 return 0;
             }
-        }
+            if (!strcmp("div", argv[2])) {
+                if (number2 != 0) {
+                    result = number1 / number2;
+                    printf("%d / %d = %d\n", number1, number2, result);
+                } else {
+                    if (number1 == 0) {
+                        printf("%d / %d = NaN\n", number1, number2);
+                    } else if (number1 > 0) {
+                        printf("%d / %d = +INF\n", number1, number2);
+                    } else {
+                        printf("%d / %d = -INF\n", number1, number2);
+                    }
+                }
+                return 0;
+            }
 
+        }
         usage(argv[0]);
 
         return 0;
